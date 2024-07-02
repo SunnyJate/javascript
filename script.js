4bee4296c99e50a339388aa1cb393ade710e1682
SunnyJate <sunnyjate2007@gmail.com>
2024-07-02 22:57:54 +0530

    nested Function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello");
+// console.log("hello");
+function fun1(){
+    console.log("I am function");
+    function fun2(){
+        console.log("I am function2");
+    }
+    fun2();
+}
+fun1();
